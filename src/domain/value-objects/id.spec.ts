6b7b165af95871ca5d323c2941d18a16405919f8
Vyctor <dev.vyctor@gmail.com>
2024-07-04 22:30:57 -0300

    implement value object id

---
+++ src/domain/value-objects/id.spec.ts
@@ -0,0 +1,21 @@
+import { Id } from "./id";
+
+describe("Uuid value object unit tests", () => {
+  it("should create a new uuid", () => {
+    const uuid = Id.create(undefined);
+
+    expect(uuid).toBeDefined();
+    expect(Id.validate(uuid.getValue())).toBe(true);
+  });
+
+  it("should create a new uuid with a value", () => {
+    const genericUuid = "550e8400-e29b-41d4-a716-446655440000";
+    const uuid = Id.create(genericUuid);
+    expect(uuid.getValue()).toBe(genericUuid);
+  });
+
+  it("should throw an error when creating a uuid with an invalid value", () => {
+    const invalidUuid = "invalid-uuid";
+    expect(() => Id.create(invalidUuid)).toThrow("Invalid id");
+  });
+});
---
+++ src/domain/value-objects/id.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid, validate } from "uuid";
+
+export class Id {
+  private constructor(private value: string) {}
+
+  static create(value?: string): Id {
+    if (!value) {
+      return new Id(uuid());
+    }
+    const isUuid = validate(value);
+
+    if (!isUuid) {
+      throw new Error("Invalid id");
+    }
+    return new Id(value);
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+
+  static validate(value: string): boolean {
+    return validate(value);
+  }
+}
