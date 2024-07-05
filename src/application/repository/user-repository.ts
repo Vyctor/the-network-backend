bae3164d08d87b2b22cc5086060b971f4b419c28
Vyctor <dev.vyctor@gmail.com>
2024-07-04 21:16:52 -0300

    implementing UserRepository interface

---
+++ src/application/repository/user-repository.ts
@@ -0,0 +1,10 @@
+import { User } from "../../domain/entities/user";
+
+export interface UserRepository {
+  create(user: User): Promise<void>;
+  update(user: User): Promise<void>;
+  list(): Promise<User[]>;
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findByNickname(nickname: string): Promise<User | null>;
+}
